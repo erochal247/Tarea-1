2cc95dddcdee5a5fa2ca1943bdea09ccdfec0578
Edwin Rocha Leon <85699211+erochal247@users.noreply.github.com>
2021-06-27 17:28:22 -0500

    Create Ejercicio1_Flujo.py

---
+++ Ejercicio1_Flujo.py
@@ -0,0 +1,12 @@
+#ejercicio 1 Encontrar la superficie de un círculo para un radio cualquiera.
+class Superficie:
+    def __init__(self):
+        pass
+
+    def su_circulo(self):
+        rad= float(input("Ingrese el radidio del circulo: "))
+        sup= 3.1416*(rad**2)
+        print("La superficie del círculo es {}" .format(sup))
+
+obje=Superficie()
+obje.su_circulo()
